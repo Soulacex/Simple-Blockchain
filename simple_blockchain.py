aadba2feb1712107b845dd4348c88ce4d43c7e76
Carolus <soulacex77@gmail.com>
2022-12-17 15:31:31 -0500

    Simple Blockchain

--- simple_blockchain.py
+++ simple_blockchain.py
@@ -1,3 +1,12 @@
+### Author: Carolus ###
+
+"""
+This code defines a Block class, which represents a single block in the blockchain. Each block has a timestamp, some data, the hash of the previous block, and a hash that is calculated from the block's data and previous hash.
+
+The Blockchain class represents the entire blockchain and contains a list of blocks. It has a method add_block that adds a new block to the blockchain, and a method get_genesis_block that returns the first block in the blockchain (also known as the "genesis block").
+
+The code creates a new Blockchain object and adds three blocks to it. Finally, it iterates over the blocks and prints their contents.
+"""
 import hashlib
 import time
 
